d3af274df23a58b2c5a18f2ad4ed93498cc5b3db
DaveCheez <davecheez21@gmail.com>
2023-04-03 01:59:18 +0100

    update env config

---
+++ base/db.py
@@ -0,0 +1,12 @@
+from base.env import config
+import dj_database_url
+
+DATABASE_URL= config("DATABASE_URL", default=None)
+if DATABASE_URL is not None:
+    DATABASES = {
+    'default': dj_database_url.config(
+        default=DATABASE_URL,
+        conn_max_age=600,
+        conn_health_checks=True
+    )
+}
