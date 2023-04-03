d202643a64e4f67b1520f7fc87350a636013976d
DaveCheez <davecheez21@gmail.com>
2023-04-03 01:25:48 +0100

    edit cart admin

--- cart/admin.py
+++ cart/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
+@admin.register(Cart)
 class CartAdmin(admin.ModelAdmin):
     list_display = ['id', 'user', 'created_at']
 
+@admin.register(CartItem)
 class CartItemAdmin(admin.ModelAdmin):
     list_display = ['id', 'cart', 'quantity' 'product']
