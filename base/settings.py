1ad8e752f2ab19b51965f37b1b3151eaf0d417d7
DaveCheez <davecheez21@gmail.com>
2023-04-11 17:52:10 +0100

    added call button

--- base/settings.py
+++ base/settings.py
@@ -93,14 +93,16 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
-STATIC_ROOT = STATIC_ROOT = BASE_DIR / "staticfiles-cdn" # dev example
 
-from cdn.conf import *  # noqa
+if DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+else:
+    from cdn.conf import *  # noqa
+    STATIC_ROOT = STATIC_ROOT = BASE_DIR / "staticfiles-cdn" # dev example
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles'),
-]
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'staticfiles'),
+    ]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
