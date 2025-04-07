0e3511de70e010ed7b9b46000855af7fe2ad98e8
Tom Haire <tom_haire@hotmail.com>
2025-04-07 19:16:07 +0100

    WIP

--- store/admin.py
+++ store/admin.py
@@ -1,24 +1,57 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Product, Category, ProductImage, CategoryImage
 
+# --- INLINE ADMIN CLASSES ---
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 1
+
+
+class CategoryImageInline(admin.TabularInline):
+    model = CategoryImage
+    extra = 1
+
+
+# --- MAIN ADMIN REGISTRATIONS ---
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug']
-    prepopulated_fields = {'slug':('name',)}
+    prepopulated_fields = {'slug': ('name',)}
+    inlines = [CategoryImageInline]
+    search_fields = ['name']
+
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug', 'price', 'created_at', 'updated_at']
-    prepopulated_fields = {'slug':('name',)}
-    
+    prepopulated_fields = {'slug': ('name',)}
+    list_filter = ['category', 'created_at']
+    search_fields = ['name', 'description']
+    inlines = [ProductImageInline]
+
+
 @admin.register(ProductImage)
 class ProductImageAdmin(admin.ModelAdmin):
-    list_display = ['name', 'slug', 'created_at', 'updated_at']
-    prepopulated_fields = {'slug':('name',)}
+    list_display = ['name', 'slug', 'image_preview', 'created_at', 'updated_at']
+    prepopulated_fields = {'slug': ('name',)}
+
+    def image_preview(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" width="50" style="object-fit:cover;" />', obj.image.url)
+        return "-"
+    image_preview.short_description = 'Preview'
 
 
 @admin.register(CategoryImage)
 class CategoryImageAdmin(admin.ModelAdmin):
-    list_display = ['name', 'slug']
-    prepopulated_fields = {'slug':('name',)}
+    list_display = ['name', 'slug', 'image_preview']
+    prepopulated_fields = {'slug': ('name',)}
+
+    def image_preview(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" width="50" style="object-fit:cover;" />', obj.image.url)
+        return "-"
+    image_preview.short_description = 'Preview'
--- store/models.py
+++ store/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.urls import reverse
-from PIL import Image
+
 
 
 class Category(models.Model):
