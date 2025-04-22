cbf947f29b1baa1f4ca43166968fa6cebde5118d
Tom Haire <tom_haire@hotmail.com>
2025-04-22 15:26:53 +0100

    Add cart ID

--- cart/models.py
+++ cart/models.py
@@ -2,15 +2,17 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 from store.models import Product
+import uuid
 
 
 class Cart(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     session_key = models.CharField(max_length=40, null=True, blank=True)
     created_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
-        return f"Cart ({self.user or self.session_key})"
+        return f"Cart ({self.user or self.session_key or self.id})"
 
 class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
